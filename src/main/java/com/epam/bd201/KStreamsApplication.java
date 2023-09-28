f36b41d9018886ffdc83d303739e8a6f734861a4
Jakub Porebski <jakub_porebski@epam.com>
2023-09-28 14:31:27 +0200

    Readme
    Beginning of the work on documentation.

--- src/main/java/com/epam/bd201/KStreamsApplication.java
+++ src/main/java/com/epam/bd201/KStreamsApplication.java
@@ -20,41 +20,42 @@ public class KStreamsApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(KStreamsApplication.class);
 
-    public static void main(String[] args) throws Exception{
+    private static final ExecutorService ste = Executors.newSingleThreadExecutor();
 
-        logger.info("Running KStreamsApplication... ");
-
-        SpecificAvroSerde<ExpediaIn> serdeExpediaIn = new SpecificAvroSerde<>();
-        serdeExpediaIn.configure(AppConfig.getSchemaRegistryConfig(), false);
-
-        SpecificAvroSerde<ExpediaOut> serdeExpediaOut = new SpecificAvroSerde<>();
-        serdeExpediaOut.configure(AppConfig.getSchemaRegistryConfig(), false);
-
-        final StreamsBuilder builder = new StreamsBuilder();
-        final KStream<Integer, ExpediaIn> inputRecords = builder.stream(AppConfig.INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), serdeExpediaIn));
-        inputRecords
-                .mapValues(Expedia::process)
-                .to(AppConfig.OUTPUT_TOPIC_NAME, Produced.with(Serdes.Integer(), serdeExpediaOut));
-
-        final Topology topology = builder.build();
-        logger.info(topology.describe().toString());
-
-        final KafkaStreams streams = new KafkaStreams(topology, AppConfig.getStreamProperties());
-
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-        ExecutorService ste = Executors.newSingleThreadExecutor();
+    public static void main(String[] args) {
 
         try {
-            // 1. run streaming operations
-            logger.info("Starting streaming operations");
-            streams.start();
+            logger.info("Running KStreamsApplication... ");
 
-            // 2. and then start copying expedia data
-            logger.info("Starting reading data from a GCP bucket");
-            ste.submit(new ExpediaDataReader()).get();
+            SpecificAvroSerde<ExpediaIn> serdeExpediaIn = new SpecificAvroSerde<>();
+            serdeExpediaIn.configure(AppConfig.getSchemaRegistryConfig(), false);
+
+            SpecificAvroSerde<ExpediaOut> serdeExpediaOut = new SpecificAvroSerde<>();
+            serdeExpediaOut.configure(AppConfig.getSchemaRegistryConfig(), false);
+
+            final StreamsBuilder builder = new StreamsBuilder();
+            final KStream<Integer, ExpediaIn> inputRecords = builder.stream(AppConfig.INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), serdeExpediaIn));
+            inputRecords
+                    .mapValues(Expedia::process)
+                    .to(AppConfig.OUTPUT_TOPIC_NAME, Produced.with(Serdes.Integer(), serdeExpediaOut));
+
+            final Topology topology = builder.build();
+            logger.info(topology.describe().toString());
+
+            try (final KafkaStreams streams = new KafkaStreams(topology, AppConfig.getStreamProperties())) {
+
+                // 1. run streaming operations
+                logger.info("Starting streaming operations");
+                streams.start();
+
+                // 2. and then start copying expedia data
+                logger.info("Starting reading data from a GCP bucket");
+                ste.submit(new ExpediaDataReader()).get();
+            }
 
             logger.info("Finished the task");
         } catch (Throwable e) {
+            logger.error("Terrible error occurred", e);
             System.exit(1);
         } finally {
             ste.shutdownNow();
