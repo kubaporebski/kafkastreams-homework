8c06158cecccac0ada341843aea06192babe1a11
Jakub Porebski <jakub_porebski@epam.com>
2023-10-03 14:23:15 +0200

    Little changes

---
+++ ksql_statements.sql
@@ -0,0 +1,28 @@
+set 'auto.offset.reset' = 'earliest';
+
+create stream expedia_ext_stream (
+	id bigint,
+	date_time varchar,
+	site_name int,
+	posa_container int,
+	user_location_country int,
+	user_location_region int,
+	user_location_city int,
+	orig_destination_distance double,
+	user_id int,
+	is_mobile int,
+	is_package int,
+	channel int,
+	srch_ci varchar,
+	srch_co varchar,
+	srch_adults_cnt int,
+	srch_children_cnt int,
+	srch_rm_cnt int,
+	srch_destination_id int,
+	srch_destination_type_id int,
+	hotel_id bigint,
+	duration varchar
+) with (
+	kafka_topic = 'expedia_ext',
+	value_format = 'avro'
+);
